b7514ac14bae022e046bf4bcfb337d4b8e24c2fa
Martin Perina <mperina@redhat.com>
2023-06-02 15:46:46 +0200

    Release 4.5.3.2
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- .automation/build-srpm.sh
+++ .automation/build-srpm.sh
@@ -11,21 +11,6 @@ ENGINE_VERSION="ovirt-engine-4.5.3.z"
 # Additional dependencies, which are going to be added to engine and which need
 # to be included in ovirt-engine-build-dependencies, so proper build can pass
 ADDITIONAL_DEPENDENCIES="
-org.yaml:snakeyaml:1.33
-com.fasterxml.jackson.core:jackson-annotations:2.12.7
-com.fasterxml.jackson.jaxrs:jackson-jaxrs-json-provider:2.12.7
-com.fasterxml.jackson.core:jackson-core:2.12.7
-com.fasterxml.jackson.core:jackson-databind:2.12.7.1
-com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.7
-org.springframework:spring-aop:5.3.27
-org.springframework:spring-beans:5.3.27
-org.springframework:spring-core:5.3.27
-org.springframework:spring-expression:5.3.27
-org.springframework:spring-instrument:5.3.27
-org.springframework:spring-jdbc:5.3.27
-org.springframework:spring-context:5.3.27
-org.springframework:spring-tx:5.3.27
-org.springframework:spring-test:5.3.27
 "
 
 # Directory, where build artifacts will be stored, should be passed as the 1st parameter
@@ -44,8 +29,8 @@ cd ovirt-engine
 git checkout ${ENGINE_VERSION}
 
 # Prepare the release, which contain git hash of engine commit and current date
-PKG_RELEASE="0.$(git rev-list HEAD | wc -l).$(date +%04Y%02m%02d%02H%02M)"
-#PKG_RELEASE="1"
+#PKG_RELEASE="0.$(git rev-list HEAD | wc -l).$(date +%04Y%02m%02d%02H%02M)"
+PKG_RELEASE="1"
 
 # Build engine project to download all dependencies to the local maven repo
 mvn \
